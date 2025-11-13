1ef846dfc2466875f4a8bbd0eccc714a795e64bf
sungmsang <sungmsang@gmail.com>
2025-11-13 18:55:34 +0900

    Update run_yolo.sh

--- run_yolo.sh
+++ run_yolo.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-set -e
-
-if [ $# -lt 1 ]; then
-  echo "사용법: docker run <이미지이름> <이미지URL>"
-  exit 1
-fi
-
-IMG_URL="$1"
-
 cd /darknet
+./darknet detect cfg/yolov3.cfg yolov3.weights $1
 
-if [ ! -f yolov3.weights ]; then
-  echo "yolov3.weights 다운로드 중..."
-  wget https://pjreddie.com/media/files/yolov3.weights
-fi
-
-echo "이미지 다운로드 중..."
-wget -O input.jpg "$IMG_URL"
-
-echo "YOLOv3 탐지 실행 중..."
-./darknet detect cfg/yolov3.cfg yolov3.weights input.jpg
